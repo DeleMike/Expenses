68f3261838f65967d6858c18d52b822f16387e88
DeleMike <akindelemichael65@gmail.com>
2020-11-24 16:04:03 +0100

    update widget tree

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -1,4 +1,5 @@
 import 'package:Expense/models/transaction.dart';
+import 'package:Expense/widgets/transaction_item.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -29,62 +30,9 @@ class TransactionList extends StatelessWidget {
         : ListView.builder(
             itemCount: userTransactions.length,
             itemBuilder: (ctx, index) {
-              return Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 16.0, vertical: 2.0),
-                child: Card(
-                  margin: EdgeInsets.symmetric(
-                    vertical: 8.0,
-                    horizontal: 5.0,
-                  ),
-                  elevation: 5.0,
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      radius: 30.0,
-                      child: Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: FittedBox(
-                          child: Text(
-                              '\$${userTransactions[index].amount.toStringAsFixed(2)}'),
-                        ),
-                      ),
-                    ),
-                    title: Text(
-                      userTransactions[index].title,
-                      style: Theme.of(context).textTheme.headline6,
-                    ),
-                    subtitle: Text(
-                      DateFormat()
-                          .add_yMMMd()
-                          .format(userTransactions[index].date),
-                      style: TextStyle(
-                        fontWeight: FontWeight.normal,
-                        fontSize: 14.0,
-                        color: Colors.grey,
-                      ),
-                    ),
-                    trailing: MediaQuery.of(context).size.width > 500
-                        ? FlatButton.icon(
-                            icon: Icon(
-                              Icons.delete,
-                            ),
-                            label: Text(
-                              'Delete',
-                            ),
-                            textColor: Theme.of(context).errorColor,
-                            onPressed: () =>
-                                deleteTx(userTransactions[index].id))
-                        : IconButton(
-                            icon: Icon(
-                              Icons.delete,
-                              color: Theme.of(context).errorColor,
-                            ),
-                            onPressed: () =>
-                                deleteTx(userTransactions[index].id)),
-                  ),
-                ),
-              );
+              return TransactionItem(transaction: userTransactions[index], deleteTx: deleteTx);
             },
           );
   }
 }
+
